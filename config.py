3a1c6e75ffab42f3850e30a0f0b385d76f4935eb
JAIDEEP <110852245+Mario22623@users.noreply.github.com>
2024-08-07 13:54:09 +0000

    Add files via upload

---
+++ config.py
@@ -0,0 +1,25 @@
+# API credentials
+API_ID = 29253709
+API_HASH = "ec295cc3d8d91a04fddfddd8a41b6227"
+BOT_TOKEN = "6398517547:AAHNm3h1VHqgljy5f3_2jIMivYcX4bKMtyM"
+
+# Redis configuration
+REDIS_CONFIG = {
+    "HOST": "redis-12345.c16.us-east-1-2.ec2.cloud.redislabs.com",
+    "PORT": 17713,
+    "USERNAME": "default",
+    "PASSWORD": "password",
+    "DATABASE": "NTM"
+}
+
+# Chat ID where videos will be stored
+PRIVATE_CHAT_ID = -100123456789
+
+# Authentication cookie
+COOKIE = {
+    "COOKIE": "csrfToken=aKum6MQzDPLa9gJhaSYV-e8U; browserid=LK9K3ZONLAzsZwdGj4UCPztMtzRKdBuNvxilHm9AcpADKHbhlONQdf5Nb7k=; lang=en; TSID=nvI85QBYy246c7RGX4NMlxP0xXwnDOoE; _ga=GA1.1.1401170140.1723036661; __bid_n=1912cf5c7aaf8537a44207; ndut_fmt=5064F2EF1C9B9437E1F7DF5A715AC892D41DB707C92C194737F21CD9D0E742FA; ab_sr=1.0.1_MWM0YWUwYjNmMmRkNTEyNjU0ZWVmYjNhNTBkY2NiZDJmZjViNTI0MzkzM2U2ZDY2ZDAxM2RkMWEwNzNjNTliZDI0ZWE4ZjExOGM5YTZmMjY1NjZjNWRlN2ZlYTE5YWU2ZmExYjU2NTkyY2FkZWJkZDdhYmUwMzdkODY4ZGE4ZDhiOTljMGZiOTBhYmIyMWJhNTFiMWE0ZTBkNmY2ZmVlZg==; _ga_06ZNKL8C2E=GS1.1.1723036660.1.1.1723036840.23.0.0"
+}
+# COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=; lang=en; TSID=; __bid_n=; _ga=; __stripe_mid=; ndus=; browserid==; ndut_fmt=; _ga_06ZNKL8C2E=" (don't use this)
+
+# List of bot admins
+ADMINS = [803003146]
